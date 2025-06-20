e78a4fc511640d4fc83304340c52ae5006d76023
Nidhi Rao <05nidzzzzz@gmail.com>
2025-06-20 18:55:27 +0530

    Update encrypt.py
    made 3 other functions, unswap_pixel, xor encrypt and decrypt

--- PixelCrypt/encrypt.py
+++ PixelCrypt/encrypt.py
@@ -7,3 +7,15 @@ def swap_pixels(pixels, key=42): #this is a function which scrambles the pixel p
    np.random.shuffle(indices) #randomly shuffles the pixel positions using the random key(seeded above). the order of the pixel indices gets randomized, it tells us where each pixel goes. this is the encryption key for pixel positions. 
    shuffled= flat[indices] #rearranges the picels based on the shuffled indices. new pixel = encrypted version.
    return shuffled.reshape(pixels.shape),indices #reshapes back to original image shape and returns the shuffled pixels along with the indices used for shuffling. the indices are needed for decryption later to know how to reverse the shuffle.
+
+def unswap_pixels(pixels,indices): #this function is essentially used for decryption, ,it takes the encrypted pixel array pixels that has its pixels shuffled and orginal shuffle indices which records how the pixels were rearranged and it reconstructs the original image by undoing the shuffle
+    flat=pixels.reshapr(-1,3) #flattens the 3D array of RGM into a 2D array. each row is now a pixel with RGB values.
+    original=np.zeros_like(flat) #creates an array of the shape shape of the flat but filled with zeros. this will hold the original values after unswapping. it is like a blank canvas on which the each correct pixel is stored
+    original[indices]=flat #uses the indices to place each pixel back in its original position. it fills the original array with the pixels in the correct order.
+    return original.reshape(pixels.shape) #reshapes the 2D array to the original image shape. 
+
+def xor_pixels(pixels,key=99): #thisfunction applies a simple XOR bitwise operation to each pixel using a key. it is a basic encryption technique that flips bits based on a key. it is used to further encrypt the pixel values after shuffling.
+    return pixels ^ key #applies the XOR operation to each pixel value with the key. it flips the bits of each pixel based on the key. this is a simple encryption step that can be reversed by applying the same XOR operation again with the same key. ^ python bitwise operator for XOR
+
+def xor_decrypt(pixels,key=99): #this function reverses the XOR operation by applying the same key again. it is used to decrypt the pixel values after shuffling.
+    return pixels ^ key #applies the XOR operation again with the same key to reverse the encryption. it flips the bits back to their original values. this is a simple decryption step that can be reversed by applying the same XOR operation again with the same key.
